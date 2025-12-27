0f6d82336153b7f75c21da62e9ecabd721dd86ae
tristan-orourke <tristan.orourke@gmail.com>
2025-12-26 22:09:12 -0500

    day 3 complete

--- src/day3.rs
+++ src/day3.rs
@@ -1,26 +1,32 @@
+#[derive(Clone)]
 pub struct BatteryBank {
-    batteries: Vec<u32>,
-    max_joltage: u32,
+    pub batteries: Vec<u64>,
+    pub max_joltage: u64,
 }
 
 impl BatteryBank {
-    pub fn new(batteries: Vec<u32>) -> BatteryBank {
+    pub fn new(batteries: Vec<u64>) -> BatteryBank {
         let (max, _, _) = BatteryBank::largest_two_digits(&batteries);
         BatteryBank {
             batteries: batteries,
-            max_joltage: max as u32,
+            max_joltage: max as u64,
         }
     }
 
-    pub fn from_string(s: &str) -> Result<BatteryBank, Box<dyn std::error::Error>> {
+    fn string_to_digits(s: &str) -> Result<Vec<u64>, Box<dyn std::error::Error>> {
         if s.chars().all(|c| c.is_digit(10)) {
-            let batteries = s.chars().map(|c| c.to_digit(10).unwrap() as u32).collect();
-            Ok(BatteryBank::new(batteries))
+            Ok(s.chars().map(|c| c.to_digit(10).unwrap() as u64).collect())
         } else {
             Err("s must be all digits".into())
         }
     }
-    fn find_max_with_pos(digits: &[u32]) -> (usize, u32) {
+
+    pub fn from_string(s: &str) -> Result<BatteryBank, Box<dyn std::error::Error>> {
+        let batteries = BatteryBank::string_to_digits(s)?;
+        Ok(BatteryBank::new(batteries))
+    }
+
+    fn find_max_with_pos(digits: &[u64]) -> (usize, u64) {
         let find_max_inner = |(i, max), (index, digit)| {
             if digit > max {
                 (index, digit)
@@ -37,32 +43,79 @@ impl BatteryBank {
     }
 
     // Returns largest two digits, and indexes of both
-    pub fn largest_two_digits(digits: &[u32]) -> (u32, usize, usize) {
-        let (i, max) = BatteryBank::find_max_with_pos(digits);
-        let (j, max_2) = BatteryBank::find_max_with_pos(&digits[(i+1)..]);
+    pub fn largest_two_digits(digits: &[u64]) -> (u64, usize, usize) {
+        let (i, max) = BatteryBank::find_max_with_pos(&digits[..digits.len() - 1]);
+        let (j, max_2) = BatteryBank::find_max_with_pos(&digits[(i + 1)..]);
 
-        (max * 10 + max_2, i, j+i+1)
+        (max * 10 + max_2, i, j + i + 1)
+    }
+
+    pub fn largest_n_digits(n: u64, digits: &[u64]) -> Vec<u64> {
+        let (i, max) = BatteryBank::find_max_with_pos(&digits[..digits.len() - (n - 1) as usize]);
+        if n == 1 {
+            vec![max]
+        } else {
+            let mut max_digits = vec![max];
+            max_digits.append(&mut BatteryBank::largest_n_digits(
+                n - 1,
+                &digits[(i + 1)..],
+            ));
+            max_digits
+        }
+    }
+    pub fn get_max_joltage(&self, n: u64) -> u64 {
+        BatteryBank::largest_n_digits(n, &self.batteries)
+            .into_iter()
+            .reduce(|acc, d| acc * 10 + d)
+            .unwrap_or(0)
     }
 }
 
 #[cfg(test)]
 mod tests_day3 {
     use super::*;
+    use std::fs;
 
     #[test]
     fn test_find_max() {
-        let digits: Vec<u32> = vec![2, 5, 0, 5];
+        let digits: Vec<u64> = vec![2, 5, 0, 5];
         assert_eq!(
             BatteryBank::find_max_with_pos(&digits),
-            (1 as usize, 5 as u32)
+            (1 as usize, 5 as u64)
         );
     }
 
     #[test]
     fn test_largest_two() {
-        let digits: Vec<u32> = vec![5, 1, 1, 5, 4];
+        let digits: Vec<u64> = vec![5, 1, 1, 5, 4];
         assert_eq!(BatteryBank::largest_two_digits(&digits), (55, 0, 3));
-        let digits: Vec<u32> = vec![5, 1, 1, 6, 4];
+        let digits: Vec<u64> = vec![5, 1, 1, 6, 4];
         assert_eq!(BatteryBank::largest_two_digits(&digits), (64, 3, 4));
+        let digits: Vec<u64> = vec![5, 1, 1, 6, 7];
+        assert_eq!(BatteryBank::largest_two_digits(&digits), (67, 3, 4));
+    }
+
+    #[test]
+    fn test_largest_n_digits() {
+        assert_eq!(
+            BatteryBank::largest_n_digits(3, &vec![5, 1, 1, 5, 4]),
+            vec![5, 5, 4]
+        );
+    }
+
+    #[test]
+    fn test_string_to_digits() {
+        assert_eq!(BatteryBank::string_to_digits("215").unwrap(), vec![2, 1, 5]);
+    }
+
+    #[test]
+    fn test_12_joltage_sum() {
+        let day3_str = fs::read_to_string("./src/data/day3_test.txt").unwrap();
+        let banks = day3_str
+            .trim()
+            .split("\n")
+            .map(|s| BatteryBank::from_string(s));
+        let joltage_sum_12: u64 = banks.map(|b| b.unwrap().get_max_joltage(12)).sum();
+        assert_eq!(joltage_sum_12, 3121910778619);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ fn main() {
     let day1 = day1_str.trim().split("\n");
     let safe = day1.fold(day1::Safe::new(), |acc, cmd| acc.run_cmd(cmd));
     println!("Resulting state is {safe:?}");
+
     let day2_str = fs::read_to_string("./src/data/day2.txt").unwrap();
     let day2_result = day2::process_ids_sum_duplicates(day2_str.trim());
     println!("Day 2 duplicate sum is {day2_result:?}");
+
+    let day3_str = fs::read_to_string("./src/data/day3.txt").unwrap();
+    let banks = day3_str.trim().split("\n").map(|s| day3::BatteryBank::from_string(s));
+    let joltage_sum: u64 = banks.clone().map(|b| b.unwrap().max_joltage).sum();
+    let joltage_sum_12: u64 = banks.map(|b| b.unwrap().get_max_joltage(12)).sum();
+    println!("Day 3 max 2-joltage sum is {joltage_sum}, 12-joltage is {joltage_sum_12}");
 }
